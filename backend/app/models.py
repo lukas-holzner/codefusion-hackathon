69661e4a70980d68240a367ec75217303db483b1
Lukas Holzner <git@lukas.lhal.de>
2024-09-30 15:18:27 +0200

    removed Items from models and schema

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,13 +3,6 @@ from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from .database import Base
 
-class Item(Base):
-    __tablename__ = "items"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    description = Column(String)
-
 class User(Base):
     __tablename__ = "users"
 
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,19 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
 
-class ItemBase(BaseModel):
-    name: str
-    description: str | None = None
-
-class ItemCreate(ItemBase):
-    pass
-
-class Item(ItemBase):
-    id: int
-
-    class Config:
-        orm_mode = True
-
 class UserBase(BaseModel):
     username: str
 
