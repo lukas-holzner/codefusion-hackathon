0e8076a5d9a384a4c57c5294c5a6ce7ec8ee2139
Lukas Holzner <git@lukas.lhal.de>
2024-09-30 13:20:59 +0200

    Added default for database env var

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./database.db")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
